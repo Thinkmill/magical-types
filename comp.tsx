8df6c14333520e8aee3173516981f9709df71f45
mitchellhamilton <mitchell@hamil.town>
2019-07-07 12:16:40 +1000

    Move all the things

---
+++ comp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { PropTypes } from "magical-types";
+
+type Props = {
+  /* Some description for this */
+  aProp: boolean;
+};
+
+let MyComponentThatDoesStuff = (props: Props) => {
+  return;
+};
+
+<PropTypes component={MyComponentThatDoesStuff} />;
---
+++ packages/magical-types/src/index.ts
@@ -0,0 +1,5 @@
+import { ComponentType } from "react";
+
+export let PropTypes = (props: { component: ComponentType }) => {
+  return null;
+};
