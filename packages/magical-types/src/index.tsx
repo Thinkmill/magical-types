b7abbe18445ef05b284c65179b08fee4b3bfe05d
mitchellhamilton <mitchell@hamil.town>
2019-07-27 11:41:44 +1000

    Move more things

--- packages/magical-types/src/index.tsx
+++ packages/magical-types/src/index.tsx
@@ -21,6 +21,7 @@ import AddBrackets, {
   bracketStyle,
   PathExpansionContext
 } from "./pretty-proptypes/PrettyConvert/AddBrackets";
+import { getChildMagicalNodes } from "./utils";
 import * as flatted from "flatted";
 
 const Arrow = () => (
@@ -455,138 +456,13 @@ function getPathsThatShouldBeExpandedByDefault(rootNode: MagicalNode) {
     // we don't want to open any nodes deeper than 5 nodes by default
     if (currentPositionedNode.path.length < 5) {
       queue.push(
-        ...getChildren(currentPositionedNode).filter(
+        ...getChildMagicalNodes(currentPositionedNode).filter(
           ({ node }) => !visitedNodes.has(node)
         )
       );
     }
   }
   return pathsThatShouldBeExpandedByDefault;
-}
-
-function getChildren({
-  node,
-  path
-}: PositionedMagicalNode): Array<PositionedMagicalNode> {
-  function getPositionedNodeFromKey<Obj, Key extends keyof Obj>(
-    obj: Obj,
-    key: Key
-  ): {
-    node: Obj[Key];
-    path: Array<string | number>;
-  } {
-    return { node: obj[key], path: path.concat(key as string | number) };
-  }
-  switch (node.type) {
-    case "StringLiteral":
-    case "NumberLiteral":
-    case "TypeParameter":
-    case "Intrinsic": {
-      return [];
-    }
-    case "Union":
-    case "Intersection": {
-      return node.types.map((node, index) => {
-        return { node, path: path.concat("types", index) };
-      });
-    }
-    case "Array":
-    case "Promise":
-    case "ReadonlyArray": {
-      return [{ node: node.value, path: path.concat("value") }];
-    }
-    case "Tuple": {
-      return node.value.map((node, index) => {
-        return { node, path: path.concat("value", index) };
-      });
-    }
-    case "IndexedAccess": {
-      return [
-        { node: node.object, path: path.concat("object") },
-        { node: node.index, path: path.concat("index") }
-      ];
-    }
-    // case "Function": {
-    //   return [
-    //     { node, path: path.concat("return") },
-    //     ...node.parameters.map((param, index) => {
-    //       return {
-    //         node: param.type,
-    //         path: path.concat("parameters", index, "type")
-    //       };
-    //     }),
-    //     ...node.typeParameters.map((node, index) => {
-    //       return { node, path: path.concat("typeParameters", index) };
-    //     })
-    //   ];
-    // }
-    case "Class": {
-      return [
-        ...node.properties.map((param, index) => {
-          return {
-            node: param.value,
-            path: path.concat("properties", index, "value")
-          };
-        })
-      ];
-    }
-    case "Object": {
-      return [
-        ...node.callSignatures
-          .map((signature, index) => {
-            return [
-              ...signature.parameters.map(x => ({
-                node: x.type,
-                path: path.concat("callSignatures", "parameters", index, "type")
-              })),
-              {
-                node: signature.return,
-                path: path.concat("callSignatures", "return")
-              }
-            ];
-          })
-          .flat(),
-        ...node.constructSignatures
-          .map((signature, index) => {
-            return [
-              ...signature.parameters.map(x => ({
-                node: x.type,
-                path: path.concat("callSignatures", "parameters", index, "type")
-              })),
-              {
-                node: signature.return,
-                path: path.concat("constructSignatures", "return")
-              }
-            ];
-          })
-          .flat(),
-        ...node.aliasTypeArguments.map((node, index) => ({
-          node,
-          path: path.concat("aliasTypeArguments", index)
-        })),
-        ...node.properties.map((param, index) => {
-          return {
-            node: param.value,
-            path: path.concat("properties", index, "value")
-          };
-        })
-      ];
-    }
-    case "Conditional": {
-      return [
-        getPositionedNodeFromKey(node, "check"),
-        getPositionedNodeFromKey(node, "true"),
-        getPositionedNodeFromKey(node, "false"),
-        getPositionedNodeFromKey(node, "extends")
-      ];
-    }
-
-    default: {
-      let _thisMakesTypeScriptEnsureThatAllNodesAreSpecifiedHere: never = node;
-      // @ts-ignore
-      throw new Error("this should never happen: " + node.type);
-    }
-  }
 }
 
 let renderTypes = (props: any) => {
