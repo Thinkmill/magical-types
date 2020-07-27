df0d739c52a6fa484f52ab64044b9ae98a47485a
mitchellhamilton <mitchell@hamil.town>
2020-07-27 10:56:22 +1000

    Rename a file so jest doesn't think it's a test file

--- test-script/src/index.ts
+++ test-script/src/index.ts
@@ -8,14 +8,16 @@ let project = new Project({
   tsConfigFilePath: path.join(__dirname, "..", "..", "tsconfig.json"),
 });
 
-let result = project.getSourceFiles("test-script/src/test.tsx").map((file) => {
-  return Object.fromEntries(
-    [...file.getExportedDeclarations().entries()].map(([name, decl]) => {
-      let type = decl[0].getType().compilerType;
-      return [name, serializeNodes([convertType(type, [])])] as const;
-    })
-  );
-});
+let result = project
+  .getSourceFiles("test-script/src/test-thing.tsx")
+  .map((file) => {
+    return Object.fromEntries(
+      [...file.getExportedDeclarations().entries()].map(([name, decl]) => {
+        let type = decl[0].getType().compilerType;
+        return [name, serializeNodes([convertType(type, [])])] as const;
+      })
+    );
+  });
 
 fs.writeFileSync(
   path.join(__dirname, "index.json"),
